22910c8eb985bfd61f6cfa1f9aa358fd08ee27f7
bobrowiczkrzysztof <69210846+bobrowiczkrzysztof@users.noreply.github.com>
2021-11-09 12:49:30 +0100

    Add Javascript

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-const firstNameEl = document.querySelector('#first-name');
-const lastNameEl = document.querySelector('#last-name');
-const emailEl = document.querySelector('#email');
-const passwordEl = document.querySelector('#password');
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+
 const form = document.querySelector('#form');
+const formField = document.querySelectorAll('.form-field');
 
-form.addEventListener('submit', e => {
+
+const isRequired = value => (value === '' ? false : true);
+
+const showError = (input, message) => {
+  const formField = input.parentElement;
+  formField.className = 'form-field error';
+  const small = formField.querySelector('small');
+  small.innerText = message;
+};
+
+const showSuccess = input => {
+  const formField = input.parentElement;
+  formField.className = 'form-field success';
+};
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+form.addEventListener('submit', function (e) {
   e.preventDefault();
-});
 
-const isRequired = value => value === '' ? false : true;
+  checkRequired([firstName, lastName, email, password]);
+  console.log(email.value);
+});
